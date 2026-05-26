a06a3ad443a21157f21f9bc473860ef5a5c83a2f
amackpro <prajwal.kumar008@gmail.com>
2026-05-26 15:37:37 +0530

    anykernel3: update

--- anykernel.sh
+++ anykernel.sh
@@ -153,22 +153,24 @@ sync
 extract_vendor_dlkm_modules_dir=${extract_vendor_dlkm_dir}/vendor_dlkm/lib/modules
 
 ui_print "- Updating /vendor_dlkm image..."
-for module in $(ls ${home}/_modules/)
+for module in $(ls ${home}/dlkm_modules/)
 do
 	if [[ -f ${extract_vendor_dlkm_modules_dir}/${module} ]]; then
 		ui_print "replacing $module"
-		cp -f ${home}/_modules/${module} ${extract_vendor_dlkm_modules_dir}/
+		cp -f ${home}/dlkm_modules/${module} ${extract_vendor_dlkm_modules_dir}/
 	else
 		if [[ $module == "goodix_core.ko" ]]; then
 			if [[ -f ${extract_vendor_dlkm_modules_dir}/goodix_ts.ko ]]; then
 				ui_print "replacing goodix_ts.ko"
-				cp -f ${home}/_modules/${module} ${extract_vendor_dlkm_modules_dir}/goodix_ts.ko
+				cp -f ${home}/dlkm_modules/${module} ${extract_vendor_dlkm_modules_dir}/goodix_ts.ko
 			fi
 		elif [[ $module == "focaltech_touch.ko" ]]; then
 			if [[ -f ${extract_vendor_dlkm_modules_dir}/focaltech_3683g.ko ]]; then
 				ui_print "replacing focaltech_3683g.ko"
-				cp -f ${home}/_modules/${module} ${extract_vendor_dlkm_modules_dir}/focaltech_3683g.ko
+				cp -f ${home}/dlkm_modules/${module} ${extract_vendor_dlkm_modules_dir}/focaltech_3683g.ko
 			fi
+		else
+			ui_print "$module not found in vendor_dlkm"
 		fi
 	fi
 done
@@ -215,28 +217,30 @@ if [ -f "$home/dtb" ]; then
   cp -f "$home/dtb" "$home/vendor_boot_extract/dtb"
 fi
 
-# if [ -d "$home/_modules" ]; then
-	# module_path="$home/vendor_boot_extract/ramdisk/lib/modules/"
-	# for module in $(ls ${home}/_modules/)
-	# do
-		# if [[ -f ${module_path}/${module} ]]; then
-			# ui_print "replacing $module"
-			# cp -f ${home}/_modules/${module} ${module_path}/
-		# else
-			# if [[ $module == "goodix_core.ko" ]]; then
-				# if [[ -f ${module_path}/goodix_ts.ko ]]; then
-					# ui_print "replacing goodix_ts.ko"
-					# cp -f ${home}/_modules/${module} ${module_path}/goodix_ts.ko
-				# fi
-			# elif [[ $module == "focaltech_touch.ko" ]]; then
-				# if [[ -f ${module_path}/focaltech_3683g.ko ]]; then
-					# ui_print "replacing focaltech_3683g.ko"
-					# cp -f ${home}/_modules/${module} ${module_path}/focaltech_3683g.ko
-				# fi
-			# fi
-		# fi
-	# done
-# fi
+if [ -d "$home/vb_modules" ]; then
+	module_path="$home/vendor_boot_extract/ramdisk/lib/modules/"
+	for module in $(ls ${home}/vb_modules/)
+	do
+		if [[ -f ${module_path}/${module} ]]; then
+			ui_print "replacing $module"
+			cp -f ${home}/vb_modules/${module} ${module_path}/
+		else
+			if [[ $module == "goodix_core.ko" ]]; then
+				if [[ -f ${module_path}/goodix_ts.ko ]]; then
+					ui_print "replacing goodix_ts.ko"
+					cp -f ${home}/vb_modules/${module} ${module_path}/goodix_ts.ko
+				fi
+			elif [[ $module == "focaltech_touch.ko" ]]; then
+				if [[ -f ${module_path}/focaltech_3683g.ko ]]; then
+					ui_print "replacing focaltech_3683g.ko"
+					cp -f ${home}/vb_modules/${module} ${module_path}/focaltech_3683g.ko
+				fi
+			else
+				ui_print "$module not found in vendor_boot"
+			fi
+		fi
+	done
+fi
 
 ui_print "- Repacking vendor_boot ramdisk..."
 cd $home/vendor_boot_extract/ramdisk
